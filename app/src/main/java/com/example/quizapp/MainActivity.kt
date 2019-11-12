ae4db4f501c0c5f5c549107e91a35e64b6ce3310
Filip <filipbanaszczyk@wp.pl>
2019-11-12 20:56:56 +0100

    First try of git. Test OnClick added
    Signed-off-by: Filip <filipbanaszczyk@wp.pl>

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Debug
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d("test_log", "testing new branch")
         setContentView(R.layout.activity_main)
+
+
+
+    }
+    fun click(view: View)
+    {
+        val toast = Toast.makeText(
+            applicationContext,
+            "próba mikrofonu",
+            Toast.LENGTH_SHORT
+        )
+
+        toast.show()
 
     }
 }
